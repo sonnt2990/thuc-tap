bbe4d5546cffd7727db6f2360e90305117b91151
sonnt2990 <sonnt@zitga.com.vn>
2021-04-29 14:22:43 +0700

    Revert "Revert "create module""
    This reverts commit 000a4cd54d10556d33436f48d298af3020ffc125.

---
+++ custom/thuc_tap/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Thực Tập ',
+    'version': '0.1',
+    'sequence': 5,
+    'category': 'Productivity',
+    'website': 'google.com',
+    'summary': 'Bán độ đua xe',
+    'description': """
+Bla bla bla... 
+""",
+    'depends': [],
+    'data': [
+
+    ],
+
+    'installable': True,
+    'application': True,
+}
