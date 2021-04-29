048450f8b5357d735848de67609fe0d89807c9ec
sonnt2990 <sonnt@zitga.com.vn>
2021-04-29 14:39:00 +0700

    start time

---
+++ custom/thuc_tap/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Thực tập',
+    'version': '0.1',
+    'sequence': 5,
+    'category': 'Productivity',
+    'website': 'google.com',
+    'summary': 'Bán độ cá cược',
+    'description': """
+Bla bla bla... 
+""",
+    'depends': [],
+    'data': [
+        'views/cargo_view.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+}
---
+++ custom/thuc_tap/models/cargo_model.py
@@ -0,0 +1,29 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class Cargo(models.Model):
+    _name = "manage.cargo"
+    _description = "Cargo"
+
+    code = fields.Char(String='code')
+
+    name = fields.Char(String='Name')
+
+    total_weight = fields.Float(String='Total weight')
+
+    length = fields.Float(String='Length')
+
+    weight = fields.Float(String='Weight')
+
+    height = fields.Float(String='Height')
+
+    from_depot = fields.Float(String='From')
+
+    to_depot = fields.Float(String='To')
+
+    total_distance = fields.Float(String='Total distance')
+
+    bidding_package_id = fields.Char(String='Bidding package')
+
+    size_standard_id = fields.Char(String='Size standard')
